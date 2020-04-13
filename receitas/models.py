d643b9675ffe4ca71727b157f71e71bc245632bb
Igor Bagio <igor.natanbagio@gmail.com>
2020-04-12 21:44:32 -0300

    Models

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    method = models.TextField()
+    ingredients = models.TextField()
+    category = models.TextField()
+    prepare_time = models.IntegerField()
+    production = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(default=datetime.now())
